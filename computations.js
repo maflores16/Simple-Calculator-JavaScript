4030ab276c583d72d4597c1560f0a0fe02dbbb0b
Mario A. Flores <maflores16@miners.utep.edu>
2014-06-12 00:08:01 -0600

    Added Comments and Changed TItle
    I added comments for better understanding of the code, plus now the
    Index.html displays the title "Simple Calculator" in your tab or
    browser.

--- computations.js
+++ computations.js
@@ -1,12 +1,24 @@
+//Global values we use in order to keep calculations in order
+
 var sum = false;
 var min = false;
 var digit = 0;
 
+//The press method gets the variable from the button
+//It also locks and unlocks the calculator whenever there's a sum or substraction
+
 function press(variable)
 {
+    //Here we get the DOM element from the screen in order to use it in JS
+    
     var screen = document.getElementById("screen");
     
+    //Then we get its current value displaying
+    
     screen.value = variable;
+    
+    //If the calculator has been locked to sum or substract, we proceed to calculate it
+    //Else, we just save the current digit on screen for future use
     
     if(sum === true){
         
@@ -24,6 +36,8 @@ function press(variable)
     }
 }
 
+//The adition method locks the calculator into sum mode
+
 function addition(){
     
     if(sum === false){        
@@ -31,6 +45,8 @@ function addition(){
     }
     
 }
+
+//The substraction method locks the calculator into substraction mode
 
 function substraction(){
     
